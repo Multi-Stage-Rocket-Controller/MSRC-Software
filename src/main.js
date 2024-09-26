ee2c00fb9a6131341f29013efc46bf53c25ac900
Matt Sherrill <98839754+mdsherrill@users.noreply.github.com>
2024-09-26 17:28:56 -0500

    Add React functionality, Add rocket icon

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '../../src/assets/rocket_icon.ico'),
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
