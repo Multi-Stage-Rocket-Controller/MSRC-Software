8880ac49b33dc24a49a6fd7e5e498538be87c790
Matt Sherrill <98839754+mdsherrill@users.noreply.github.com>
2024-10-16 16:41:42 -0500

    Add conextIsolation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -12,13 +12,14 @@ const createWindow = () => {
     height: 1200,
     icon: path.join(__dirname, '../../src/../public/assets/rocket_icon.ico'),
     webPreferences: {
+      contextIsolation: true,  // Prevents React from accessing Electron
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
     autoHideMenuBar: true,
   });
 
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  mainWindow.webContents.openDevTools(); // Open DevTools
+  mainWindow.webContents.openDevTools(); // Opens DevTools (Ctrl + Shift + I)
 };
 
 app.whenReady().then(() => {
